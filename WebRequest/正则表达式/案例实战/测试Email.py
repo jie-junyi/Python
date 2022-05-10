b62ab3f1b872e656f5ca63e9d3f0b46e31ac1615
jie-junyi <3274861360@qq.com>
2022-05-10 15:29:47 +0800

    f

--- WebRequest/正则表达式/案例实战/测试Email.py
+++ WebRequest/正则表达式/案例实战/测试Email.py
@@ -7,13 +7,15 @@ bill@ieee.cn，请确认输入的Email是否正确！']
    输出：匹配每个字符串中的邮箱email，并输出格式为：str的匹配结果为：['lining@geekori.com']
 
 """
-if __name__ == '__main__':
-    strList = 'lining@geekori.com abcedfg@aa 我的邮箱是jfzeng@ccnu.edu.cn，不是bill@ieee.cn，请确认输入的Email是否正确！'
-    ptn1 = r'.*com'
-    #  比上面的难搞一点点
-    ptn2 = r'[a-z@\.]*cn'
-    matchList1 = re.findall(ptn1, strList, re.I)
-    matchList2 = re.findall(ptn2, strList, re.I)
 
-    print(matchList1)
-    print(matchList2)
+
+def EmailMatch(email):
+    ptn3 = r'[a-z]*@[a-z.]*(?:cn|com)'
+    matchList = re.findall(ptn3, email, re.I)
+    print(email, " 的匹配结果为：", matchList)
+
+
+if __name__ == '__main__':
+    strList2 = ['lining@geekori.com', 'abcedfg@aa', '我的邮箱是jfzeng@ccnu.edu.cn，不是bill @ ieee.cn，请确认输入的Email是否正确！']
+    for i in strList2:
+        EmailMatch(i)
