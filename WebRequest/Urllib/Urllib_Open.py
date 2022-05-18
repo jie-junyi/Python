1159c4642f131974629ee1be4cd97adb6f210e4d
jie-junyi <3274861360@qq.com>
2022-05-18 23:51:24 +0800

    最高分

--- WebRequest/Urllib/Urllib_Open.py
+++ WebRequest/Urllib/Urllib_Open.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     # 返回"http.client.HTTPResponse”对象。
     try:
         data = bytes(urllib.parse.urlencode({'hello': 'python'}), encoding='utf-8')
-        response = urllib.request.urlopen(url='https://www.baidu.com/', data=data,timeout=1)
+        response = urllib.request.urlopen(url='https://www.baidu.com/', data=data, timeout=1)
         # 读取全部的
         print(response.read().decode('utf-8'))
     except urllib.error.URLError as error:
